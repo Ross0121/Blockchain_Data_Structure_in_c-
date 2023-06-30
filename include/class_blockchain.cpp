46b1362bee294c587d2f7c25a0776b7e94671aed
Rushikesh Langde <74859623+Ross0121@users.noreply.github.com>
2023-06-30 19:30:07 +0530

    Create class_blockchain.cpp

---
+++ include/class_blockchain.cpp
@@ -0,0 +1,17 @@
+class Blockchain
+{
+private:
+    Block createGenesisBlock();
+    std::vector<Block> chain;
+
+public:
+    // Constuctor
+    Blockchain();
+    
+    // Public Functions
+    std::vector<Block> getChain();
+    Block *getLatestBlock();
+    bool isChainValid();
+    void addBlock(TransactionData data);
+    void printChain();
+};
