2e5e49cd6e05d71196a943ca48ef7d4c4384b623
Rushikesh Langde <74859623+Ross0121@users.noreply.github.com>
2023-06-30 19:31:03 +0530

    Create struct_TransactionData.cpp

---
+++ include/struct_TransactionData.cpp
@@ -0,0 +1,17 @@
+struct TransactionData
+{
+    double amount;
+    std::string senderKey;
+    std::string receiverKey;
+    time_t timestamp;
+    
+    TransactionData(){};
+    
+    TransactionData(double amt, std::string sender, std::string receiver, time_t time)
+    {
+        amount = amt;
+        senderKey = sender;
+        receiverKey = receiver;
+        timestamp = time;
+    };
+};
